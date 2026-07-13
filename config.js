49079a3b318babadf94d7098bad54ce32fa7c29a
mario <20210050@ricaldone.edu.sv>
2026-07-13 15:24:00 -0600

    AAAAA

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
-export const config ={
-    db:{
-        uri:process.env.DB_URI
-    },
-    jwt:{
-        secret:process.env.JWT_SECRET_KEY
-    },
-    email:{
-        user_email: process.env.USER_EMAIL,
-        user_password: process.env.USER_PASSWORD
-    },
+export const config = {
+  db: {
+    uri: process.env.DB_URI,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET_KEY,
+  },
+  email: {
+    user_email: process.env.USER_EMAIL,
+    user_password: process.env.USER_PASSWORD,
+  },
+  wompi: {
+    grant_type: process.env.GRANT_TYPE,
+    audience: process.env.AUDIENCE,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+  },
 };
